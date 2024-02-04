20cf08779d2ca45653be9c0fb5298c86cd04253d
Rameshwar Dudhe <141106013+rndudhe1808@users.noreply.github.com>
2024-02-04 11:40:35 +0530

    Create vars.tf

---
+++ vars.tf
@@ -0,0 +1,19 @@
+variable "pv_name" {
+  default = "example-volumename"
+}
+
+variable "pvc_name" {
+  default = "example-claimname"
+}
+
+variable "deployment_name_my_app" {
+  default = "my-app-deployment"
+}
+
+variable "deployment_name_wordpress" {
+  default = "wordpress-deployment"
+}
+
+variable "service_name_wordpress" {
+  default = "wordpress-service"
+}
