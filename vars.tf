f5386296af8712fab77833fba95be4954fbf0916
Rameshwar Dudhe <141106013+rndudhe1808@users.noreply.github.com>
2024-02-04 11:41:38 +0530

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -17,3 +17,6 @@ variable "deployment_name_wordpress" {
 variable "service_name_wordpress" {
   default = "wordpress-service"
 }
+#Firstly Configured the NFS System and change in nfs section of main.tf
+#then just do terraform init and terraform plan
+#teraform command for custom variable "terraform apply -var="pv_name=task-pv" -var="pvc_name=task-pvc" -var="deployment_name_my_app=db-dep" -var="deployment_name_wordpress=wp-dep" -var="service_name_wordpress=wp-svc" -auto-approve"
